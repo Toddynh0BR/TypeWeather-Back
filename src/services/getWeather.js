364b75b4e66effd777d783dab7e076ecafd26cbe
Toddynh0BR <galaxyplay41@gmail.com>
2026-03-11 16:43:02 -0300

    Correção de chamadas a api

--- src/services/getWeather.js
+++ src/services/getWeather.js
@@ -5,12 +5,14 @@ async function GetWeather(lat, lon) {
     const climaResponse = await axios.get(
       'https://api.openweathermap.org/data/3.0/onecall',
       {
-      lat,
-      lon,
-      exclude: 'minutely,hourly',
-      lang: 'pt_br',
-      appid: "0051ff2b54f1ae466e72cb622eff4bc1",
-    }
+        params: {
+          lat,
+          lon,
+          exclude: 'minutely,hourly',
+          lang: 'pt_br',
+          appid: "0051ff2b54f1ae466e72cb622eff4bc1",
+        }
+      }
     );
 
     return climaResponse.data;
